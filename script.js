ea2dfe5d949660b74bf817e618e4e4a14655ed00
MEHEDI HASAN TUHIN <mehedihasan547822@gmail.com>
2026-05-27 19:52:21 +0600

    script.js

--- script.js
+++ script.js
@@ -123,11 +123,11 @@ function updateForecastUI(list) {
 }
 
 
+
 async function fetchNews() {
     try {
-
-// Replace line 128 with this:
-const res = await fetch(`https://gnews.io/api/v4/search?q=weather%20OR%20climate&lang=en&max=5&apikey=${3cd9e80c850b047f1dfd28b6d233b9fa}`);
+        
+        const res = await fetch(`https://gnews.io/api/v4/search?q=weather%20OR%20climate&lang=en&max=5&apikey=3cd9e80c850b047f1dfd28b6d233b9fa`);
         const data = await res.json();
         
         if (data.articles) {
@@ -150,7 +150,7 @@ const res = await fetch(`https://gnews.io/api/v4/search?q=weather%20OR%20climate
     }
 }
 
-// Utility: Calculate exact local time
+
 function updateLocalTime(offsetSeconds) {
     const d = new Date();
     const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
